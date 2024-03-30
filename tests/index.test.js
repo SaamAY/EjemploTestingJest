c003064e6ac0a641955a51589383fbda74611fd4
Yulisa Azurdia <yulyazurdia8@gmail.com>
2024-03-30 17:53:32 -0600

    Ejemplos de testing get y post

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,7 +6,39 @@ import request from "supertest";
 describe("GET /tasks", () => {
     test("should respond with a 200 status code", async () => {
         const response = await request(app).get("/tasks").send();
-        console.log(response)
         expect(response.statusCode).toBe(200);
+    });
+
+    test("should respond with an array", async () => {
+        const response = await request(app).get("/tasks").send();
+        expect(response.body).toBeInstanceOf(Array);
+    });
+}); 
+
+describe("POST /tasks", () => {
+    describe("given a title and description", () => {
+        const newTask = {
+            title: "test task",
+            description: "test description"
+        };
+
+        test("should respond with a 200 status code", async () => {
+            const response = await request(app).post("/tasks").send(newTask);
+            expect(response.statusCode).toBe(200);
+        });
+    
+        test("should have a content-type: application/json on header", async () => {
+            const response = await request(app).post("/tasks").send(newTask);
+            expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
+        });
+    
+        test("should respond with an task ID", async () => {
+            const response = await request(app).post("/tasks").send(newTask);
+            expect(response.body.id).toBeDefined();
+        });
+    });
+
+    describe("when title and description is missing", () => {
+
     });
 }); 
